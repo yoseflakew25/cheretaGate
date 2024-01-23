9afb76c2244b5315135093ad0eefdab396f07298
yosef lakew <80267669+yoseflakew25@users.noreply.github.com>
2024-01-23 10:16:34 +0300

    .

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,51 @@
-import React from 'react'
+import {  Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import About from './pages/About';
+import Faqq from './pages/Faqq';
+import Tenders from './pages/Tenders';
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
+import Profile from './pages/Profile';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import Error from './pages/Error';
+import Dashboard from './pages/Dashboard';
+import CreateTender from './pages/CreateTender';
+import Verify from './pages/Verify'
 
-const App = () => {
+function App() {
+
   return (
-    <h1 className="text-3xl font-bold underline">
-    Hello world!
-  </h1>
-  )
+    <>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-up" element={<SignUp />} />
+
+        <Route path="/about" element={<About />} />
+        <Route path="/faq" element={<Faqq />} />
+
+        <Route path="/user/verify-email" element={<Verify />} />
+
+
+        <Route path="/tenders" element={<Tenders />}/>
+        <Route path="/profile" element={<Profile />}/>
+        <Route path="/dashboard" element={<Dashboard />}/>
+        <Route path="/createtender" element={<CreateTender />}/>
+
+   
+       
+
+        <Route path='*' element={<Error />} />
+      </Routes>
+      <ToastContainer position='top-center' />
+    <Footer />
+    </>
+  );
 }
 
-export default App
+
+export default App;
